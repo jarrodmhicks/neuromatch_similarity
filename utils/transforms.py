91eaef8dc679ccd18824d13714330d8115c74cb0
Jarrod Hicks <jmhicks@mit.edu>
2023-07-18 17:38:28 -0400

    added example simple transformations

---
+++ utils/transforms.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+
+class IdentityTransform(nn.Module):
+  def __init__(self):
+    super(IdentityTransform, self).__init__()
+
+  def forward(self, features):
+    return features # return features
+
+class DiagonalLinearTransform(nn.Module):
+  def __init__(self, n_features, sigma=0.01):
+    super(DiagonalLinearTransform, self).__init__()
+    self.diagonal = nn.Parameter(torch.randn(n_features) * sigma)
+
+  def forward(self, features):
+    return features * self.diagonal # equivalent to features @ torch.diag(self.diagonal)
+
+class FullLinearTransform(nn.Module):
+  def __init__(self, n_features, sigma=0.01):
+    super(FullLinearTransform, self).__init__()
+    self.transformation_matrix = nn.Parameter(torch.randn(n_features, n_features) * sigma)
+
+  def forward(self, features):
+    return features @ self.transformation_matrix
+
+# TODO: neural network transforms can go here
