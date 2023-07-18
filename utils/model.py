1dc77db78654c5c51a52aa0e764e5fda696f5977
Jarrod Hicks <jmhicks@mit.edu>
2023-07-18 17:39:58 -0400

    added main distance model

---
+++ utils/model.py
@@ -0,0 +1,14 @@
+import torch
+
+class DistanceModel(nn.Module):
+  def __init__(self, transform, distance):
+    super(DistanceModel, self).__init__()
+    self.transform = transform
+    self.distance = distance
+
+  def forward(self, features): # input features: batch dim by 3 by n features
+    transformed_features = self.transform(features) # batch dim by 3 by k features
+    distances = torch.hstack((torch.unsqueeze(self.distance(transformed_features[:, 1, :], transformed_features[:, 2, :]), 1),
+                              torch.unsqueeze(self.distance(transformed_features[:, 0, :], transformed_features[:, 2, :]), 1),
+                              torch.unsqueeze(self.distance(transformed_features[:, 0, :], transformed_features[:, 1, :]), 1)))
+    return -distances # batch dim by 3; (-) to convert distance to "similarity" to work with softmax in CrossEntropy loss
