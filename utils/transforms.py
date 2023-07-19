cb38fcfa4d97382f5bfe33c6260ccbbdff543f20
Jarrod Hicks <jmhicks@mit.edu>
2023-07-19 09:24:03 -0400

    renamed classes

--- utils/transforms.py
+++ utils/transforms.py
@@ -1,24 +1,24 @@
 import torch
 from torch import nn
 
-class IdentityTransform(nn.Module):
+class Identity(nn.Module):
   def __init__(self):
-    super(IdentityTransform, self).__init__()
+    super(Identity, self).__init__()
 
   def forward(self, features):
     return features # return features
 
-class DiagonalLinearTransform(nn.Module):
+class DiagonalLinear(nn.Module):
   def __init__(self, n_features, sigma=0.01):
-    super(DiagonalLinearTransform, self).__init__()
+    super(DiagonalLinear, self).__init__()
     self.diagonal = nn.Parameter(torch.randn(n_features) * sigma)
 
   def forward(self, features):
     return features * self.diagonal # equivalent to features @ torch.diag(self.diagonal)
 
-class FullLinearTransform(nn.Module):
+class FullLinear(nn.Module):
   def __init__(self, n_features, sigma=0.01):
-    super(FullLinearTransform, self).__init__()
+    super(FullLinear, self).__init__()
     self.transformation_matrix = nn.Parameter(torch.randn(n_features, n_features) * sigma)
 
   def forward(self, features):
@@ -27,9 +27,9 @@ class FullLinearTransform(nn.Module):
 class LinearProjection(nn.Module):
   def __init__(self, in_features, out_features, sigma=0.01):
     super(LinearProjection, self).__init__()
-    self.transformation_matrix = nn.Parameter(torch.randn(in_features, out_features) * sigma)
+    self.projection_matrix = nn.Parameter(torch.randn(in_features, out_features) * sigma)
 
   def forward(self, features):
-    return features @ self.transformation_matrix
+    return features @ self.projection_matrix
 
 # TODO: neural network transforms can go here
