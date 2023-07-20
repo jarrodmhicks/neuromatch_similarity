e7f8ac6e79daefbeb4f4fafcdb62c6b1646a98d8
Jarrod Hicks <jmhicks@mit.edu>
2023-07-19 21:33:13 -0400

    added euclidean distance and simplified call

--- utils/distances.py
+++ utils/distances.py
@@ -1,12 +1,15 @@
 import torch
 from torch import nn
 
-class CosineDistance(nn.Module):
+class Cosine(nn.Module):
   def __init__(self):
-    super(CosineDistance, self).__init__()
+    super(Cosine, self).__init__()
     self.cos_sim = nn.CosineSimilarity(dim=1)
 
   def forward(self, input1, input2):
     return 1 - self.cos_sim(input1, input2)
+
+def Euclidean():
+  return nn.PairwiseDistance(p=2)
 
 # TODO: other distance metrics can go here
