8adffb54eeefad675c31482c562b2ff1dd2c8b3e
Jarrod Hicks <jmhicks@mit.edu>
2023-07-19 21:32:13 -0400

    moved functions from helpers to model

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,35 +1,13 @@
 import torch
-
-def train_model(features, model, optimizer, loss_function, train_loader, summary=True):
-  n_steps = len(train_loader)
-  for i, (triplets, targets) in enumerate(train_loader):
-    inputs = features[triplets,:]
-
-    # forward pass
-    outputs = model(inputs)
-    loss = loss_function(outputs, targets)
-
-    # backward and optimization step
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-    # print summary on last step of epoch
-    if ((i+1) == n_steps) & summary:
-      print(f'\tStep [{i+1}/{n_steps}], Loss: {loss.item():.6f}')
-
-  return loss.item()
-
-def model_performance(outputs, targets):
-  model_decisions = torch.argmax(outputs, dim=1)
-  performance = torch.sum(model_decisions == targets) / len(targets)
-  return performance.item()
+import os.path as osp
 
 def set_device():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f'Current device: {device}')
     return device
 
+def relative(relative_path):    
+    return osp.join(osp.dirname(osp.abspath(__file__)), relative_path)
 
 # TODO: write saving utility
 '''
--- utils/model.py
+++ utils/model.py
@@ -13,3 +13,34 @@ class DistanceModel(nn.Module):
                               torch.unsqueeze(self.distance(transformed_features[:, 0, :], transformed_features[:, 2, :]), 1),
                               torch.unsqueeze(self.distance(transformed_features[:, 0, :], transformed_features[:, 1, :]), 1)))
     return -distances # batch dim by 3; (-) to convert distance to "similarity" to work with softmax in CrossEntropy loss
+
+def model_performance(outputs, targets):
+  model_decisions = torch.argmax(outputs, dim=1)
+  performance = torch.sum(model_decisions == targets) / len(targets)
+  return performance.item()
+
+def train_model(features, model, optimizer, loss_function, train_loader, summary=True):
+  n_steps = len(train_loader)
+  for i, (triplets, targets) in enumerate(train_loader):
+    inputs = features[triplets,:]
+    outputs = model(inputs)
+    loss = loss_function(outputs, targets)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    # print summary on last step of epoch
+    if ((i+1) == n_steps) & summary:
+      print(f'\tStep [{i+1}/{n_steps}], Loss: {loss.item():.6f}')
+  return loss.item()
+
+def train(features, model, optimizer, loss_function, train_loader, num_epochs, summary_every):
+  train_losses = []
+  for epoch in range(num_epochs):
+    if (epoch+1) % summary_every == 0:
+      print(f'Epoch [{epoch+1}/{num_epochs}]')
+      summary = True
+    else:
+      summary = False
+    train_loss = train_model(features, model, optimizer, loss_function, train_loader, summary=summary)
+    train_losses.append(train_loss)
+  return train_losses
