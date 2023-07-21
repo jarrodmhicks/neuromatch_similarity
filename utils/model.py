1c45f909976ed71da90298f40bcef0df7912b3b3
Jarrod Hicks <jmhicks@mit.edu>
2023-07-21 16:50:29 -0400

    name change to be more explicit

--- utils/model.py
+++ utils/model.py
@@ -19,7 +19,7 @@ def model_performance(outputs, targets):
   performance = torch.sum(model_decisions == targets) / len(targets)
   return performance.item()
 
-def train_model(features, model, optimizer, loss_function, train_loader, summary=True):
+def train_epoch(features, model, optimizer, loss_function, train_loader, summary=True):
   n_steps = len(train_loader)
   for i, (triplets, targets) in enumerate(train_loader):
     inputs = features[triplets,:]
@@ -41,6 +41,6 @@ def train(features, model, optimizer, loss_function, train_loader, num_epochs, s
       summary = True
     else:
       summary = False
-    train_loss = train_model(features, model, optimizer, loss_function, train_loader, summary=summary)
+    train_loss = train_epoch(features, model, optimizer, loss_function, train_loader, summary=summary)
     train_losses.append(train_loss)
   return train_losses
